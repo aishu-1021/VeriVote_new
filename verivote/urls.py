dec556ce3186cdedd09c1b54c089ff1797119ff2
arzooshaikh <arzooshaikh2005@gmail.com>
2026-06-21 15:45:49 +0530

    Added Verivote modification

--- verivote/urls.py
+++ verivote/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('voters.urls')),
+    path('admin/',          admin.site.urls),
+    path('api/',            include('voters.urls')),
+    path('api/booth/',      include('booth.urls')),
+    path('api/fraud/',      include('fraud_detection.urls')),
+    path('api/chain/',      include('audit_chain.urls')),
+    path('api/federated/',  include('federated.urls')),
 ]
